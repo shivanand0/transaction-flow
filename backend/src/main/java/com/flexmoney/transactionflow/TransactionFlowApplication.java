afd14af7b264bd69af21cd1c5bf512a4f74a9081
shivanand0 <amanmitkar@gmail.com>
2023-02-13 16:05:12 +0530

    spring boot backend init

---
+++ backend/src/main/java/com/flexmoney/transactionflow/TransactionFlowApplication.java
@@ -0,0 +1,13 @@
+package com.flexmoney.transactionflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flexmoney/transactionflow/TransactionFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexmoney.transactionflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
