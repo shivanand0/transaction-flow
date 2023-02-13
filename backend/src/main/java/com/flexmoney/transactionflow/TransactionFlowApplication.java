eedc103533bc4946da497129dfa4a5f2a3e32e12
rohithgadu <gadurohith@gmail.com>
2023-02-13 18:04:36 +0530

    Added Hello World

--- backend/src/main/java/com/flexmoney/transactionflow/TransactionFlowApplication.java
+++ backend/src/main/java/com/flexmoney/transactionflow/TransactionFlowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TransactionFlowApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(TransactionFlowApplication.class, args);
 	}
 
