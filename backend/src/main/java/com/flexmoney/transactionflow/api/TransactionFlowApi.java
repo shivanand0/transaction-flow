16bededf0341453d5797b7dc819582edfde2d36b
Sanya Zaveri <zaverisanya@gmail.com>
2023-02-21 16:57:51 +0530

    Update TransactionFlowApi.java

--- backend/src/main/java/com/flexmoney/transactionflow/api/TransactionFlowApi.java
+++ backend/src/main/java/com/flexmoney/transactionflow/api/TransactionFlowApi.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 public class TransactionFlowApi {
         @Autowired
         private ITransactionFlowService transactionFlowService;
