9e2ecaac57f207621541b33ce287ce4fe718fbe2
shivanand0 <shivanand.mitkar@flexmoney.in>
2024-01-08 15:30:33 +0530

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,66 @@
-import {Routes, Route} from 'react-router-dom'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import Home from './pages/Home/Home'
-import LenderSelection from './pages/Lender/Lender/LenderSelection'
-import Error_404 from './pages/Error/Error_404'
-import Alert from './components/Alert/Alert'
-import TwoFAuthentication from './pages/TwoFactorAuthentication/TwoFAuthentication'
-import TenureSelection from "./pages/Tenure/TenureSelection.jsx";
-import TransactionStatus from "./pages/TransactionStatus/TransactionStatus.jsx";
-import TransactionComplete from './pages/TransactionComplete/TransactionComplete'
+import Home from './pages/Home/Home';
+import LenderSelection from './pages/Lender/Lender/LenderSelection';
+import Error_404 from './pages/Error/Error_404';
+import Alert from './components/Alert/Alert';
+import TwoFAuthentication from './pages/TwoFactorAuthentication/TwoFAuthentication';
+import TenureSelection from './pages/Tenure/TenureSelection.jsx';
+import TransactionStatus from './pages/TransactionStatus/TransactionStatus.jsx';
+import TransactionComplete from './pages/TransactionComplete/TransactionComplete';
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route exact path="/transaction/lender-selection/:detailsId" element={<LenderSelection/>}/>
-                <Route exact path="/transaction/tenure-selection/:detailsId" element={<TenureSelection/>}/>
-                <Route exact path="/transaction/confirm/:detailsId" element={<TwoFAuthentication />} />
-                <Route exact path="/transaction/payment/:txnStatus" element={<TransactionStatus/>}/>
-                <Route exact path="/transaction/complete" element={<TransactionComplete/>}/>
-                <Route path="*" element={<Error_404/>}/>
-            </Routes>
-            <Alert/>
-        </>
-    )
+  const check = () => {
+
+    fetch('https://test.instacred.me/app/affordability/telesales/test')
+      .then((response) => {
+        console.log(response);
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  check();
+
+  return (
+    <>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route
+          exact
+          path='/transaction/lender-selection/:detailsId'
+          element={<LenderSelection />}
+        />
+        <Route
+          exact
+          path='/transaction/tenure-selection/:detailsId'
+          element={<TenureSelection />}
+        />
+        <Route
+          exact
+          path='/transaction/confirm/:detailsId'
+          element={<TwoFAuthentication />}
+        />
+        <Route
+          exact
+          path='/transaction/payment/:txnStatus'
+          element={<TransactionStatus />}
+        />
+        <Route
+          exact
+          path='/transaction/complete'
+          element={<TransactionComplete />}
+        />
+        <Route path='*' element={<Error_404 />} />
+      </Routes>
+      <Alert />
+    </>
+  );
 }
 
-export default App
+export default App;
